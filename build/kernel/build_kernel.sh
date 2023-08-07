5f82689e6e8f5205dfc0831d728c57813ab6b6e9
Ilya <iliya2001@mail.ru>
2023-08-07 13:06:38 +0300

    correct scripts

--- build/kernel/build_kernel.sh
+++ build/kernel/build_kernel.sh
@@ -4,8 +4,11 @@ cd linux* 2>~/../../dev/null
 
 if [ "$?" -ne 0 ]
 then
-	echo "kernel: directory does not exist"
-	exit
+	echo "kernel: directory does not exist and will be downloaded"
+	wget . https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.6.tar.xz
+	tar -xvf linux*.tar*
+	rm *.tar*
+	cd linux*
 fi
 
 make defconfig
